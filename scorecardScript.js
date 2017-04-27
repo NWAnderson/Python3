95e0dd39d0852b49707cb3cdd67ca68cb388bcca
jlramones <jennicaramones@gmail.com>
2017-04-27 12:27:04 -0600

    Add files via upload

--- scorecardScript.js
+++ scorecardScript.js
@@ -15,7 +15,7 @@ function makeScorecard(){
 	var scorecard_container = document.getElementById("scorecard");
 	numOfPlayers = parseInt(document.getElementById("playerNumField").value);
 	/* Table load  */
-	if (numOfPlayers == 1 || numOfPlayers <= 4){
+	if (numOfPlayers >= 1 && numOfPlayers <= 4){
 		var thead = "<table>\n";
 		var tbody = '<td><b>Hole</b></td>';
 		for (var columnNum = 1; columnNum < numOfColumns; columnNum++){
@@ -25,11 +25,15 @@ function makeScorecard(){
 		}
 		for (var i = 1; i <= numOfPlayers; ++i){
 			tbody += '<tr class="playerRow">';
-			tbody += '<td><input id="playerCell" value="Player"></td>';
+			tbody += '<td><input id="playerCell" value="Player ';
+			tbody += i;
+			tbody += '"</td>';
 			for (var j = 1; j < numOfColumns; j++){
 				tbody += '<td>';
-				tbody += '<input id ="scoreCell" value ="1">';
-				tbody += '</td>';
+				tbody += '<input id ="scoreCell" class="scoreCell"';
+				tbody += 'j" value ="';
+				tbody += j;
+				tbody += '"></td>';
 			}
 			tbody += '</tr>\n';
 		}
@@ -49,14 +53,16 @@ function makeScorecard(){
 
 function calculateScore(){
 	var totalScore = 0;
+	totalScore = parseInt(document.getElementById("scoreCell").value);
+	document.writeln(totalScore);
 	var result_container = document.getElementById("calculatedResult");
 	var scorecard_container = document.getElementById("scorecard");
-	for (var i = 2; i < numOfPlayers; i++){
-		document.writeln("3234523");
-		for (var j = 2; j < numOfColumns; j++){
-			totalScore += parseInt(scorecard_container.table.tr[i].td[j]).value;
-		}
-	}
-	result_container.innerHTML = "Your total score is " + totalScore;
+	// for (var i = 2; i < numOfPlayers; i++){
+		// document.writeln("3234523");
+		// for (var j = 2; j < numOfColumns; j++){
+			// totalScore += parseInt(scorecard_container.table.tr[i].td[j]).value;
+		// }
+	// }
+	// result_container.innerHTML = "Your total score is " + totalScore;
 }
 window.addEventListener("load", loadButtons, false);
